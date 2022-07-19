672686823f62f604a12770464001cc5a1c02ab99
chirag110 <chiragguglani@yahoo.in>
2022-07-19 16:26:10 +0530

    Added DBContext for DB Middleware

---
+++ IMDBAPI/Data/IMDBApiContext.cs
@@ -0,0 +1,20 @@
+﻿using IMDBAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IMDBAPI.Data
+{
+    public class IMDBApiContext : DbContext
+    {
+        public IMDBApiContext(DbContextOptions<IMDBApiContext>options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+    }
+}
