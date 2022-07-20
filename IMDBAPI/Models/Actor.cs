230586d7608bd486b67863808f280554f728efc8
chirag110 <chiragguglani@yahoo.in>
2022-07-20 14:50:18 +0530

    Minor Optimizations

--- IMDBAPI/Models/Actor.cs
+++ IMDBAPI/Models/Actor.cs
@@ -8,7 +8,7 @@ namespace IMDBAPI.Models
     public class Actor
     {
         public int ActorId { get; set; }
-        //public IList<Movie> Movies { get; set; }
+        public IList<Movie> Movies { get; set; }
         public string ActorName { get; set; }
 
         public List<Actor_Movie> Actor_Movies { get; set; }
